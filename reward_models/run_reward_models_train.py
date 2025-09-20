2373432b1eb27ef13f0c584e44f66d2d1dbf5f4c
Brian Christian <brian.christian@psy.ox.ac.uk>
2025-09-20 18:00:18 +0100

    add max_steps parameter to trainers

--- reward_models/run_reward_models_train.py
+++ reward_models/run_reward_models_train.py
@@ -25,6 +25,7 @@ class ScriptArguments:
     gradient_accumulation_steps: Optional[int] = field(default=16)
     learning_rate: Optional[float] = field(default=1e-5)
     num_train_epochs: Optional[int] = field(default=2, metadata={"help": "The number of training epochs for the reward model."})
+    max_steps: Optional[int] = field(default=-1, metadata={"help": "Total number of optimizer steps to train for. Overrides num_train_epochs."})
     optim: Optional[str] = field(default="adamw_hf",  metadata={"help": "The optimizer to use."})
     lr_scheduler_type: Optional[str] = field(default="cosine", metadata={"help": "The lr scheduler"},)
     max_length: Optional[int] = field(default=1024) 
@@ -94,6 +95,7 @@ training_args = TrainingArguments(
     per_device_train_batch_size=script_args.per_device_train_batch_size,
     per_device_eval_batch_size=script_args.per_device_eval_batch_size,
     num_train_epochs=script_args.num_train_epochs,
+    max_steps=script_args.max_steps,
     evaluation_strategy=script_args.evaluation_strategy,
     eval_steps=script_args.eval_steps,
     save_strategy=script_args.save_strategy,
